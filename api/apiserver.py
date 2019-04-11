296d2f7eb5da00162a8e777355576fa2c9f25c4c
Calum Boal <cgboal521@gmail.com>
2019-04-11 16:04:36 +0000

    Added initial docker integration, also added parsing of env variables in yml files

--- api/apiserver.py
+++ api/apiserver.py
@@ -28,6 +28,19 @@ from binascii import a2b_base64
 
 logging.basicConfig(filename="logs/detailed.log",level=logging.DEBUG)
 
+path_matcher = re.compile(r'\$\{([^}^{]+)\}')
+
+def path_constructor(loader, node):
+  ''' Extract the matched value, expand env variable, and replace the match '''
+  value = node.value
+  match = path_matcher.match(value)
+  env_var = match.group()[2:-1]
+  return os.environ.get(env_var) + value[match.end():]
+
+yaml.add_implicit_resolver('!path', path_matcher)
+yaml.add_constructor('!path', path_constructor)
+
+
 try:
     with open( '../config.yaml', 'r' ) as f:
         settings = yaml.safe_load( f )
